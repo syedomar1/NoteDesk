591cd5898afc4cd030f38ccdd6099028892c2729
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2023-12-31 23:37:06 +0530

    Added Font Awesome

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -6,7 +6,11 @@ const NoteItem = (props) => {
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
+          <div className="d-flex align-items-center">
+            <h5 className="card-title">{note.title}</h5>
+            <i className="far fa-trash-alt mx-2"></i>
+            <i className="far fa-edit mx-2"></i>
+          </div>
           <p className="card-text">{note.description}</p>
         </div>
       </div>
