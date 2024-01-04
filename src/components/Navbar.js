bc553db57182b3db9c886af8be70169978f5553c
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-01-04 17:25:03 +0530

    Navbar changes made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
   //   console.log(location.pathname );
   // }, [location]);
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">NoteDesk</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
