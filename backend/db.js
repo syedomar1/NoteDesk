cb233a0c9956dcd0963f0e25486da4614b8dee8f
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-01-05 18:59:19 +0530

    db port updated

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://0.0.0.0:27017/notedesk"
+const mongoURI = "mongodb://127.0.0.1:27017/notedesk"
 const connectToMongo=()=>{
     mongoose.connect(mongoURI);
     console.log("Connected to mongoose successfully");
